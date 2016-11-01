3d40f78ec0594b4b35d83bd16169d408766db564
Robin Millette <robin@millette.info>
2016-11-01 18:06:52 -0400

    Fix tests.

--- test/server/web/index.js
+++ test/server/web/index.js
@@ -6,6 +6,7 @@ const Config = require('../../../config')
 const Hapi = require('hapi')
 const Vision = require('vision')
 const Inert = require('inert')
+const I18N = require('hapi-i18n')
 const HomePlugin = require('../../../server/web/index')
 
 const lab = exports.lab = Lab.script()
@@ -16,6 +17,22 @@ lab.beforeEach((done) => {
   const plugins = [Inert, Vision, HomePlugin]
   server = new Hapi.Server()
   server.connection({ port: Config.get('/port/web') })
+
+  server.register(
+    {
+      register: I18N,
+      options: {
+        locales: ['fr', 'en'],
+        directory: 'locales'
+      }
+    },
+    function (err) {
+      if (err) {
+        return done(err)
+      }
+    }
+  )
+
   server.register(plugins, (err) => {
     if (err) {
       return done(err)
@@ -34,7 +51,27 @@ lab.experiment('Home Page View', () => {
   lab.beforeEach((done) => {
     request = {
       method: 'GET',
-      url: '/'
+      url: '/fr/'
+    }
+
+    done()
+  })
+
+  lab.test('home page renders properly (fr)', (done) => {
+    server.inject(request, (response) => {
+      Code.expect(response.result).to.match(/Bienvenue à la démonstration hapi\/lodash/i)
+      Code.expect(response.statusCode).to.equal(200)
+
+      done()
+    })
+  })
+})
+
+lab.experiment('Home Page View (en)', () => {
+  lab.beforeEach((done) => {
+    request = {
+      method: 'GET',
+      url: '/en/'
     }
 
     done()
@@ -42,7 +79,7 @@ lab.experiment('Home Page View', () => {
 
   lab.test('home page renders properly', (done) => {
     server.inject(request, (response) => {
-      Code.expect(response.result).to.match(/activate the plot device/i)
+      Code.expect(response.result).to.match(/Welcome to the hapi\/lodash demonstration/i)
       Code.expect(response.statusCode).to.equal(200)
 
       done()
