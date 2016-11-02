47e02a314319952b17e3666e1ae292d915b64ab7
Robin Millette <robin@millette.info>
2016-11-02 08:06:02 -0400

    Removed old stuff.

--- server/web/index.js
+++ server/web/index.js
@@ -3,42 +3,12 @@
 exports.register = function (server, options, next) {
   const list1 = require('../../data/list1.json')
   const list2 = require('../../data/list2.json')
-  const pages = [
-    'abonnez-vous',
-    'accueil',
-    'accueil-tous',
-    'agenda',
-    'allo',
-    'apropos',
-    'bottin',
-    'bye',
-    'faq',
-    'index',
-    'moi',
-    'page-ext-1',
-    'page-int-1',
-    'resultats',
-    'section-acteurs',
-    'section-actionnaires',
-    'section-entreprise',
-    'sujet-acericulture',
-    'sujet-agriculture',
-    'tests'
-  ]
 
   server.views({
     engines: { html: require('lodash-vision') },
     path: 'templates',
     partialsPath: 'templates/partials',
     isCached: options.templateCached
-  })
-
-  pages.forEach((page) => {
-    server.route({
-      method: 'GET',
-      path: '/{languageCode}/' + page,
-      handler: { view: page }
-    })
   })
 
   server.route({
